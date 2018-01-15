224821b706989b7bbb04f7c8e0b66e43c93aa69c
Steven Kelsey <steveace6@Stevens-MacBook-Pro.local>
2018-01-15 13:44:38 -0500

    hn tracker api intial

---
+++ src/test/java/com/hackernight/hntrackerapi/HnTrackerApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hackernight.hntrackerapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HnTrackerApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
